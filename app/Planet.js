c1a5195588afedb41ce6199677aab63ebab2078f
Spookiss <jbeucher@et.esiea.fr>
2024-01-29 21:46:28 +0100

    Correction deploy(4)

--- app/Planet.js
+++ app/Planet.js
@@ -46,31 +46,31 @@ const Planet = () => {
   return (
     <div className="relative w-120 h-120">
       <div className="absolute inset-1/4 flex items-center justify-center">
-        <Image src="/planet.png" alt="Planet" width={300} height={300} />
+        <Image src="./planet.png" alt="Planet" width={300} height={300} />
       </div>
       <div className="absolute top-0 left-1/2 transform  small-planet top-center">
-        <Image src="/github.png" alt="Small Planet" width={100} height={100} />
+        <Image src="./github.png" alt="Small Planet" width={100} height={100} />
       </div>
       <div className="absolute bottom-20 left-20 transform  small-planet ">
-        <Image src="/small-planet.png" alt="Small Planet" width={100} height={100} />
+        <Image src="./small-planet.png" alt="Small Planet" width={100} height={100} />
       </div>
       <div className="absolute bottom-0 left-1/2 transform  small-planet bottom-center ">
-        <Image src="/small-planet.png" alt="Small Planet" width={100} height={100} />
+        <Image src="./small-planet.png" alt="Small Planet" width={100} height={100} />
       </div>
       <div className="absolute bottom-20 right-20 transform small-planet ">
-        <Image src="/small-planet.png" alt="Small Planet" width={100} height={100} />
+        <Image src="./small-planet.png" alt="Small Planet" width={100} height={100} />
       </div>
       <div className="absolute left-0 top-1/2 transform  small-planet left-center ">
-        <Image src="/small-planet.png" alt="Small Planet" width={100} height={100} />
+        <Image src="./small-planet.png" alt="Small Planet" width={100} height={100} />
       </div>
       <div className="absolute left-20 top-20 transform small-planet ">
-        <Image src="/small-planet.png" alt="Small Planet" width={100} height={100} />
+        <Image src="./small-planet.png" alt="Small Planet" width={100} height={100} />
       </div>
       <div className="absolute right-0 top-1/2 transform  small-planet right-center ">
-        <Image src="/small-planet.png" alt="Small Planet" width={100} height={100} />
+        <Image src="./small-planet.png" alt="Small Planet" width={100} height={100} />
       </div>
       <div className="absolute right-20 top-20 transform small-planet ">
-        <Image src="/small-planet.png" alt="Small Planet" width={100} height={100} />
+        <Image src="../small-planet.png" alt="Small Planet" width={100} height={100} />
       </div>
     </div>
   );
